80bc4fa27300756caa9f6f18d8e59b2c6ae39026
KIK <2779179028@qq.com>
2026-01-30 17:18:23 +0800

    后端的初始化 1.springbott3.5.4 2.knfie4j 3.基本的工具类

---
+++ src/main/java/com/yu/yuaicodemother/YuAdiCodeMotherApplication.java
@@ -0,0 +1,13 @@
+package com.yu.yuaicodemother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuAdiCodeMotherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuAdiCodeMotherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yu/yuaicodemother/YuAdiCodeMotherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yu.yuaicodemother;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuAdiCodeMotherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
